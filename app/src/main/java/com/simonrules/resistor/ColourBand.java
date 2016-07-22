a7f6e70413ad362efb343014ef98b8b532ecf1e4
Simon Wilson <simonrules@users.noreply.github.com>
2016-07-22 06:18:20 -0600

    WIP: some ColourBand implementation

--- app/src/main/java/com/simonrules/resistor/ColourBand.java
+++ app/src/main/java/com/simonrules/resistor/ColourBand.java
@@ -4,23 +4,47 @@ import android.graphics.Color;
 
 public class ColourBand {
     public enum Colour {
-        BLACK,
-        BROWN,
-        RED,
-        ORANGE,
-        YELLOW,
-        GREEN,
-        BLUE,
-        VIOLET,
-        GREY,
-        WHITE,
-        GOLD,
-        SILVER;
+        BLACK(Color.parseColor("#000000")),
+        BROWN(Color.parseColor("#774400")),
+        RED(Color.parseColor("#FF0000")),
+        ORANGE(Color.parseColor("#FF7700")),
+        YELLOW(Color.parseColor("#FFFF00")),
+        GREEN(Color.parseColor("#00FF00")),
+        BLUE(Color.parseColor("#0000FF")),
+        VIOLET(Color.parseColor("#800080")),
+        GREY(Color.parseColor("#666666")),
+        WHITE(Color.parseColor("#FFFFFF")),
+        GOLD(Color.parseColor("#FFD700")),
+        SILVER(Color.parseColor("#C0C0C0"));
 
+        private final int colour;
+
+        Colour(int value) {
+            colour = value;
+        }
+
+        public int getColour() {
+            return colour;
+        }
     }
 
-    ColourBand(int colour) {
+    private Colour colour;
 
+    /*
+     * Iterate through all Colour enums and determine the closest colour in
+     * euclidean distance.
+     */
+    ColourBand(int colourToMatch) {
+        colour = null;
+
+        double min = 9999.9;
+        for (Colour c: Colour.values()) {
+            double dist = distance(c.getColour(), colourToMatch);
+            if (dist < min) {
+                min = dist;
+                colour = c;
+            }
+        }
     }
 
     private double distance(int colourA, int colourB) {
@@ -31,32 +55,7 @@ public class ColourBand {
         return Math.sqrt(dr * dr + dg * dg + db * db);
     }
 
-    /*final int BAND_BLACK = Color.parseColor("#000000");
-    final int BAND_BROWN = Color.parseColor("#774400");
-    final int BAND_RED = Color.parseColor("#FF0000");
-    final int BAND_ORANGE = Color.parseColor("#FF7700");
-    final int BAND_YELLOW = Color.parseColor("#FFFF00");
-    final int BAND_GREEN = Color.parseColor("#00FF00");
-    final int BAND_BLUE = Color.parseColor("#0000FF");
-    final int BAND_VIOLET = Color.parseColor("#800080");
-    final int BAND_GREY = Color.parseColor("#666666");
-    final int BAND_WHITE = Color.parseColor("#FFFFFF");
-    final int BAND_GOLD = Color.parseColor("#FFD700");
-    final int BAND_SILVER = Color.parseColor("#C0C0C0");*/
-
-
-
-    private int bandColour;
-
-    public ColourBand(int color) {
-        // Determine closest colour in euclidean distance
-        for (:
-             ) {
-            
-        }
-    }
-
-    public int getColour() {
-        return bandColour;
+    public Colour getColour() {
+        return colour;
     }
 }
--- app/src/main/java/com/simonrules/resistor/Resistor.java
+++ app/src/main/java/com/simonrules/resistor/Resistor.java
@@ -1,8 +1,5 @@
 package com.simonrules.resistor;
 
-/**
- * Created by simon on 7/20/16.
- */
 public class Resistor {
     ColourBand[] colourBands;
 }
